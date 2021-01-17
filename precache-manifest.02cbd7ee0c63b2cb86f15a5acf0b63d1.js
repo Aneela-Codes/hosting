216f885ca50a543cd770853df495df64a917380c
Aneela-Codes <aneelareiko@gmail.com>
2021-01-17 09:45:44 -0800

    Updates

---
+++ precache-manifest.02cbd7ee0c63b2cb86f15a5acf0b63d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32981437b80fc0bd31522a0d64fe91a2",
+    "url": "/hosting/index.html"
+  },
+  {
+    "revision": "46f1c52da8e40779c41c",
+    "url": "/hosting/static/css/main.10caae71.chunk.css"
+  },
+  {
+    "revision": "c893ad92880ac9b91d67",
+    "url": "/hosting/static/js/2.70551fa3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/hosting/static/js/2.70551fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46f1c52da8e40779c41c",
+    "url": "/hosting/static/js/main.1208f3b1.chunk.js"
+  },
+  {
+    "revision": "87078a1996fb62018bfe",
+    "url": "/hosting/static/js/runtime-main.da980023.js"
+  }
+]);
